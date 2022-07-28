f8a99c4246e6b05f4e721a6a71bdcede19fd8064
DESKTOP-SA0VI34\Seop <point455@bankq.co.kr>
2022-07-28 18:14:49 +0900

    컨트랙트 주석 추가

--- contracts/AdditionGame.sol
+++ contracts/AdditionGame.sol
@@ -3,20 +3,26 @@ pragma solidity ^0.4.24;
 contract AdditionGame {
     address public owner;
 
-    constructor() public {
+    constructor() public{
         owner = msg.sender;
     }
-    
-    function getBalance() public view returns (uint) {
+
+    //단위 peb , 클레이튼 가상머신에서는 최소단위인 peb로 변환해서 처리하기 때문.
+    function getBalance() public view returns(uint){
         return address(this).balance;
     }
 
-    function deposit() public payable {  
+    //계정에서 솔리디티함수로 클레이튼을 보낼때 payable 필수.
+    function deposit() public payable{
+        //송신자 유효성 체크.
         require(msg.sender == owner);
-    }   
-  
-    function transfer(uint _value) public returns (bool) {
+    }
+
+    //정답을 맞춘 사용자에게 상금을 전송하는 로직
+    function transfer(uint _value) public returns(bool){
+        //잔액확인
         require(getBalance() >= _value);
+        //전송
         msg.sender.transfer(_value);
         return true;
     }
